f162df566ce270538ed24e873a83be15eed8c156
tarunsamuel7 <52186437+tarunsamuel7@users.noreply.github.com>
2023-09-24 01:44:47 +0530

    refactor v1

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,18 @@
+from battery.battery import Battery
+import pandas as pd
+
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        next_service_date = pd.to_datetime(self.last_service_date) + pd.DateOffset(
+            years=4
+        )
+
+        if next_service_date < self.current_date:
+            return False
+
+        return True
